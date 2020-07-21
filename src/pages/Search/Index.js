4394dc3668fc4f0859d6e1f0df685d0bcdb7bf9d
matheusf7 <matheusfreire77@gmail.com>
2020-07-21 16:46:44 +0100

    Added Formik in the Search page, starting to work in the search logical

--- src/pages/Search/Index.js
+++ src/pages/Search/Index.js
@@ -1,74 +1,90 @@
-import React, {useContext} from 'react';
-import "./styles.css";
-import {Context} from "../../Context"
+import React, { useContext, useState, useEffect } from 'react';
+import './styles.css';
+import { Context } from '../../Context';
+import { Formik, Field, Form } from 'formik';
+import axios from 'axios';
 
 export default function Search() {
-  
+  const [shoePreferences, setShoePreferences] = useState({});
 
-  function onChange(e) {
-    const { name, value } = e.target;
-    setSubmitedShoe({...submitedShoe, [name]: value})
-    console.log(submitedShoe)
+  useEffect(() => {
+    if (shoePreferences) {
+      console.log('deu certo');
+    }
+  }, [shoePreferences]);
+
+  function onSubmit(values, actions) {
+    setShoePreferences(values);
+    console.log();
   }
-  
 
-  const {searchSubmit, handleSearchChange, submitedShoe, setSubmitedShoe} = useContext(Context)
-  
   return (
-    <div>
-      <div className="Search-Title">
-        <h1>Selecione as características do seu Tenis</h1>
-      </div>
-      <div className="Search-Container">
-        <h2>Drop desejado (MM)</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <form onSubmit={searchSubmit}>
-
-          <select onChange={onChange} name="drop" required>
-            <option value="4">4 Milímetros</option>
-            <option value="6">6 Milímetros</option>
-            <option value="8">8 Milímetros</option>
-            <option value="10">10 Milímetros</option>
-            <option value="12">12 MMilímetros</option>
-           </select>
-
-          <h2>Peso desejado (g)</h2>
-          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-         
-        <select onChange={onChange} name="weight" required>
-          <option value="leve">Leve</option>
-          <option value="medio">Médio</option>
-          <option value="pesado">Pesado</option>
-        </select>
-          <h2>Tipo de amortecimento</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                 
-          <select onChange={onChange} name="damping" required>
-            <option value="leve">Leve</option>
-            <option value="macio">Macio</option>
-            <option value="extramacio">Extra Macio</option>
-          </select>
-
-          <h2>Tipo de Treino</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-
-         <select onChange={onChange} name="training" >
-          <option value="running">Corrida</option>
-          <option value="rodagem">Rodagem</option>
-          <option value="competição">Competição</option>
-        </select>
-          <br />
-          <button type="submit" value="submit">Pesquisar</button>
-          
-        </form>
-       
-
-
-
-
-      </div>
-
-    </div>
-     
-    );
+    <Formik
+      onSubmit={onSubmit}
+      validateOnMount
+      initialValues={{
+        drop: 0,
+        weight: '',
+        damping: '',
+        training: '',
+      }}
+      render={({ values, errors }) => (
+        <Form>
+          <div>
+            <div className="Search-Title">
+              <h1>Selecione as características do seu Tenis</h1>
+            </div>
+            <div className="Search-Container">
+              <h2>Drop desejado (MM)</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <Field as="select" name="drop" required>
+                <option value="4">4 Milímetros</option>
+                <option value="6">6 Milímetros</option>
+                <option value="8">8 Milímetros</option>
+                <option value="10">10 Milímetros</option>
+                <option value="12">12 MMilímetros</option>
+              </Field>
+              <h2>Peso desejado (g)</h2>
+              <p>
+                {' '}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <Field as="select" name="weight" required>
+                <option value="leve">Leve</option>
+                <option value="medio">Médio</option>
+                <option value="pesado">Pesado</option>
+              </Field>
+              <h2>Tipo de amortecimento</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <Field as="select" name="damping" required>
+                <option value="leve">Leve</option>
+                <option value="macio">Macio</option>
+                <option value="extramacio">Extra Macio</option>
+              </Field>
+              <h2>Tipo de Treino</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <Field as="select" name="training">
+                <option value="running">Corrida</option>
+                <option value="rodagem">Rodagem</option>
+                <option value="competição">Competição</option>
+              </Field>
+              <button type="submit" value="submit">
+                Pesquisar
+              </button>
+            </div>
+          </div>
+        </Form>
+      )}
+    />
+  );
 }
--- src/pages/Shoes/Index.js
+++ src/pages/Shoes/Index.js
@@ -1,38 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import Firebase from "../../utils/Firebase"
-import { Context } from "../../Context"
-import "./styles.css";
+import Firebase from '../../utils/Firebase';
+import { Context } from '../../Context';
+import './styles.css';
 
 export default function Shoes() {
   const [tenis, setTenis] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const db = Firebase.firestore()
-      db.collection("Tenis")
+      const db = Firebase.firestore();
+      db.collection('Tenis')
         .get()
-        .then(querySnapshot => {
-          const data = querySnapshot.docs.map(doc => doc.data());
-          setTenis(data)
-          console.log(data)
+        .then((querySnapshot) => {
+          const data = querySnapshot.docs.map((doc) => doc.data());
+          setTenis(data);
+          console.log(data);
         });
-    }
+    };
     fetchData();
+  }, []);
 
-  }, [])
-
-  const shoeList = tenis.map(shoe => {
+  const shoeList = tenis.map((shoe) => {
     return (
       <ul>
         <li>{shoe?.values.shoe}</li>
       </ul>
-    )
-  })
+    );
+  });
 
-  return (
-    <div className="Shoes-Container">
-      {shoeList}
-    </div>
-
-  );
+  return <div className="Shoes-Container">{shoeList}</div>;
 }
